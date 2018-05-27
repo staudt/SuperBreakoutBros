8d28fa4c57559a84e415efb5583a18aca71d435e
staudt <ricardost@gmail.com>
2018-05-27 07:40:59 -0500

    collision bug fix

--- js/actors/ball.js
+++ js/actors/ball.js
@@ -12,23 +12,30 @@ class Ball extends Sprite {
     this.setMaxSpeedX(12);
     this.setMaxSpeedY(8);
     this.setBoundary();
+    this.brickCollisions = [];
   }
 
   update() {
     //this.setSpeedY(this.speedY+0.05);    
     if (this.speedY>3) this.setSpeedY(this.speedY-0.05)
     if (this.speedY<-3) this.setSpeedY(this.speedY+0.05)
-
+    if (this.brickCollisions.length>1) {
+      this.bounceFrom(this.getCollision(this.brickCollisions[0].union(this.brickCollisions[1])));
+      this.brickCollisions[0].onBounce();
+    } else if (this.brickCollisions.length > 0) {
+      this.bounceFrom(this.getCollision(this.brickCollisions[0]));
+      this.brickCollisions[0].onBounce();
+    }
+    this.brickCollisions = [];
   }
 
   onCollision(target) {
     let collision = this.getCollision(target);
-    if(target.hasTag('tile') || target.hasTag('monster')) {
-      /*if ((collision.top || collision.bottom) &&
-          (collision.left || collision.right)) {
-      }*/
+    if(target.hasTag('monster')) {
       target.onBounce();
+      this.bounceFrom(collision);
+    } else if (target.hasTag('tile')) {
+      this.brickCollisions.push(target);
     }
-    this.bounceFrom(collision);
   }
 }
